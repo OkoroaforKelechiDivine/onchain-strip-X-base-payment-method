dbb1e932a9dc99bf88944d371928f9365a071d9a
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-01 23:17:58 +0000

    created a new file

---
+++ lib/app.dart
@@ -0,0 +1,23 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get/get_navigation/src/routes/transitions_type.dart';
+import 'package:pay_me_mobile/screens/splash/splash_screen.dart';
+
+import 'app_config/manager/theme_manager.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      defaultTransition: Transition.fadeIn,
+      transitionDuration: const Duration(microseconds: 20),
+      debugShowCheckedModeBanner: false,
+      title: 'PayMe',
+      theme: getApplicationTheme(),
+      home: const SplashScreen(),
+    );
+  }
+}
