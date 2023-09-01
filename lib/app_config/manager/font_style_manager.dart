3af6308eaa8bbd803c4b14b077efa50021db4bc0
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-01 23:19:35 +0000

    created a new file: Font style manager

---
+++ lib/app_config/manager/font_style_manager.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'font_manager.dart';
+
+
+TextStyle _getAlegreyaTextStyle(double fontSize, FontWeight fontWeight, Color fontColor,){
+  return GoogleFonts.alegreya(fontSize: fontSize, fontWeight: fontWeight, color: fontColor);
+}
+
+getBodyLargeStyle({double fontSize = AppFontSize.size24, required Color color}) {
+  return _getAlegreyaTextStyle(fontSize, AppFontWeight.bold, color);
+}
+
+getBodyMediumBoldStyle(
+    {double fontSize = AppFontSize.size16, required Color color}) {
+  return _getAlegreyaTextStyle(fontSize, AppFontWeight.semiBold, color);
+}
+
+getBodyMediumRegularStyle(
+    {double fontSize = AppFontSize.size14, required Color color}) {
+  return _getAlegreyaTextStyle(fontSize, AppFontWeight.regular, color);
+}
+
+getBodySmallStyle({double fontSize = AppFontSize.size12, required Color color}) {
+  return _getAlegreyaTextStyle(fontSize, AppFontWeight.medium, color);
+}
+
+getButtonLabelStyle({double fontSize = AppFontSize.size10, required Color color}) {
+  return _getAlegreyaTextStyle(fontSize, AppFontWeight.semiBold, color);
+}
+
