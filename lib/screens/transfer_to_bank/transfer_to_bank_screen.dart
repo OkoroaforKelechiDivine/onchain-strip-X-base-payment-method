8d69fce70d5626d334945c9b01a5d773eeb9266d
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-04 05:23:45 +0000

    added leading icon in the app bar

--- lib/screens/transfer_to_bank/transfer_to_bank_screen.dart
+++ lib/screens/transfer_to_bank/transfer_to_bank_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:pay_me_mobile/app_config/manager/font_manager.dart';
 import 'package:pay_me_mobile/app_config/manager/theme_manager.dart';
 
 class TransferToBankScreen extends StatefulWidget {
@@ -16,28 +15,15 @@ class _TransferToBankScreenState extends State<TransferToBankScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
         title: const Text("Transfer to Bank", style: TextStyle(color: AppColors.lightGreen)),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          color: AppColors.lightBlue,
-          padding: const EdgeInsets.all(16.0),
-          child: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Recipient Account",
-                style: TextStyle(
-                  color: AppColors.lightBlack,
-                  fontSize: 20.0,
-                  fontWeight: AppFontWeight.bold
-                ),
-              ),
-             ]
-          ),
-        ),
-      ),
+      body: const Text("Transfer to Bank")
     );
   }
 }
