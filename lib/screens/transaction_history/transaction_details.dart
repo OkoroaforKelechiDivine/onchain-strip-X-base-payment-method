5163546178994aa235db54c5c979c55b6b65ebc3
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-09 03:26:01 +0000

    finished working on transaction history screen

---
+++ lib/screens/transaction_history/transaction_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class TransactionDetailsScreen extends StatefulWidget {
+  const TransactionDetailsScreen({super.key});
+
+  @override
+  State<TransactionDetailsScreen> createState() => _TransactionDetailsScreenState();
+}
+
+class _TransactionDetailsScreenState extends State<TransactionDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Hello Nigeria");
+  }
+}
