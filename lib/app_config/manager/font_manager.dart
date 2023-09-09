9757345adb175bb7f97289b2258a2557156073b5
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-09 13:29:12 +0000

    remove thick bold font weight

--- lib/app_config/manager/font_manager.dart
+++ lib/app_config/manager/font_manager.dart
@@ -6,7 +6,6 @@ class AppFontWeight{
   static const FontWeight medium = FontWeight.w500;
   static const FontWeight semiBold = FontWeight.w600;
   static const FontWeight bold = FontWeight.w700;
-  static const FontWeight thickBold = FontWeight.w900;
 }
 
 class AppFontSize{
