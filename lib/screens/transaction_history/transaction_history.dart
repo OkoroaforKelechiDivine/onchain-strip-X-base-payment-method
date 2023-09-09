43d5d74c83227d2d8a2f398873eebb0511854238
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-09 03:26:09 +0000

    finished working on transaction history screen

---
+++ lib/screens/transaction_history/transaction_history.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:pay_me_mobile/app_config/manager/font_manager.dart';
+import 'package:pay_me_mobile/app_config/manager/theme_manager.dart';
+
+import '../../views/custom/custom_bottom_bar_navigation.dart';
+import '../transfer_to_bank/transfer_to_bank_screen.dart';
+
+class TransactionHistoryScreen extends StatefulWidget {
+  const TransactionHistoryScreen({Key? key}) : super(key: key);
+
+  @override
+  _TransactionHistoryScreenState createState() => _TransactionHistoryScreenState();
+}
+
+class _TransactionHistoryScreenState extends State<TransactionHistoryScreen> {
+  String formatTimestamp(DateTime timestamp) {
+    return DateFormat('HH:mm').format(timestamp);
+  }
+
+  int _currentIndex = 0;
+
+
+  PreferredSizeWidget buildAppBar() {
+    return AppBar(
+      title: const Center(
+        child: Text(
+          'Transaction History',
+          style: TextStyle(
+            color: AppColors.lightGreen,
+            fontSize: AppFontSize.size24,
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  Widget buildTransactionCard(String text) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(5.0),
+            side: const BorderSide(
+              color: AppColors.lightGreen,
+              width: 2.0,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              text,
+              style: CustomStyles.transactionHistoryTextStyle,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget buildTransactionCards() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 5.0, top: 10),
+      child: SizedBox(
+        height: 80.0,
+        child: Row(
+          children: [
+            buildTransactionCard("Transfer"),
+            buildTransactionCard("Airtime"),
+            buildTransactionCard("Buy Power"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildTransactionList() {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10, left: 15.0, right: 10.0),
+        child: ListView.builder(
+          itemCount: dummyBanks.length,
+          itemBuilder: (BuildContext context, int index) {
+            DummyBank bank = dummyBanks[index];
+            return GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, "/transaction_details");
+              },
+              child: Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 3.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10.0),
+                    child: Container(
+                      color: AppColors.lightBlue,
+                      child: ListTile(
+                        leading: Image.asset(
+                          bank.logo,
+                          width: 40.0,
+                          height: 40.0,
+                        ),
+                        title: Text(
+                          bank.accountName,
+                          style: TextStyle(
+                            fontFamily: GoogleFonts.alegreyaSans().fontFamily,
+                            fontWeight: AppFontWeight.bold,
+                            fontSize: AppFontSize.size16,
+                          ),
+                        ),
+                        subtitle: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              formatTimestamp(bank.timestamp),
+                              style: const TextStyle(
+                                color: AppColors.lightBlack,
+                                fontWeight: AppFontWeight.bold,
+                                fontSize: AppFontSize.size16,
+                              ),
+                            ),
+                            const Text(
+                              "₦20,000.00",
+                              style: TextStyle(
+                                color: AppColors.lightGreen,
+                                fontWeight: AppFontWeight.bold,
+                                fontSize: AppFontSize.size16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: Column(
+        children: [
+          buildTransactionCards(),
+          buildTransactionList(),
+        ],
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class CustomStyles {
+  static const TextStyle transactionHistoryTextStyle = TextStyle(
+    fontSize: AppFontSize.size18,
+    color: AppColors.lightGreen,
+    fontWeight: AppFontWeight.light,
+  );
+}
