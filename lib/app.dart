5d95753d8c2865933133931b1ecd2d8a6f07dd4d
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-10 23:29:13 +0000

    added raise payment route

--- lib/app.dart
+++ lib/app.dart
@@ -5,6 +5,7 @@ import 'package:get/get_navigation/src/routes/transitions_type.dart';
 import 'package:pay_me_mobile/screens/buy_airtime/buy_airtime_screen.dart';
 import 'package:pay_me_mobile/screens/buy_power/buy_power_screen.dart';
 import 'package:pay_me_mobile/screens/home_page/home_page_screen.dart';
+import 'package:pay_me_mobile/screens/raise_payment/raise_payment.dart';
 import 'package:pay_me_mobile/screens/splash/splash_screen.dart';
 import 'package:pay_me_mobile/screens/transaction_history/repeat_transaction.dart';
 import 'package:pay_me_mobile/screens/transaction_history/transaction_details.dart';
@@ -34,6 +35,7 @@ class MyApp extends StatelessWidget {
         "/home": (context) => const HomePageScreen(),
         "/login": (context) => const LoginScreen(),
         "/splash": (context) => const SplashScreen(),
+        "/raise_payment": (context) => const RaisePaymentScreen(),
         "/buy_power": (context) => const BuyPowerScreen(),
         "/transfer": (context) => const TransferToBankScreen(),
         "/show_all_beneficiaries": (context) => ShowAllBeneficiariesScreen(banks: dummyBanks),
