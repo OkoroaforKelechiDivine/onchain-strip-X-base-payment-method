db85805594d18f8a3724f438d162fcdfcfb0d958
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-11 19:08:35 +0000

    switched from Get to Navigator

--- lib/controllers/splash/Splash_controller.dart
+++ lib/controllers/splash/Splash_controller.dart
@@ -1,13 +1,11 @@
 import 'dart:async';
-import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 import '../../views/auth_view/login_view.dart';
 
-class SplashController extends GetxController {
-  @override
-  void onInit() {
-    super.onInit();
-    Timer(const Duration(milliseconds: 4000), (){
-      Get.offAll(() => LoginScreen());
+class SplashController {
+  void initialize(BuildContext context) {
+    Timer(const Duration(milliseconds: 4000), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const LoginScreen()));
     });
   }
 }
