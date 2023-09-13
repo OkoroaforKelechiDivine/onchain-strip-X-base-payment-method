7bb7324c89cc4912ee60f9d0156b325fbfbed450
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-13 21:42:06 +0000

    changed splash routes

--- lib/controllers/splash/Splash_controller.dart
+++ lib/controllers/splash/Splash_controller.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:pay_me_mobile/views/auth_view/pincode.dart';
 import '../../views/auth_view/login_view.dart';
 
 class SplashController {
   void initialize(BuildContext context) {
     Timer(const Duration(milliseconds: 4000), () {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const LoginScreen()));
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => PinCodeScreen()));
     });
   }
 }
