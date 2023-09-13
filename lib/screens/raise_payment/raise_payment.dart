588f11a385c93ece024e1f6e34390d65fd0fe032
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-13 03:06:02 +0000

    created raise payment class

---
+++ lib/screens/raise_payment/raise_payment.dart
@@ -0,0 +1,577 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pay_me_mobile/app_config/manager/font_manager.dart';
+import 'package:pay_me_mobile/app_config/manager/theme_manager.dart';
+import 'package:pay_me_mobile/screens/raise_payment/raise_payment_detail.dart';
+
+import '../../views/custom/custom_bottom_bar_navigation.dart';
+import '../transfer_to_bank/transfer_to_bank_screen.dart';
+
+class RaisePaymentScreen extends StatefulWidget {
+  const RaisePaymentScreen({Key? key}) : super(key: key);
+
+  @override
+  _RaisePaymentScreenState createState() => _RaisePaymentScreenState();
+}
+
+class _RaisePaymentScreenState extends State<RaisePaymentScreen> {
+  bool _bankSelected = false;
+  bool _isAccountNumberErrorVisible = true;
+  bool _isAccountNumberLengthInvalid = false;
+  String _userName = '';
+
+  String _selectedBankLogo = '';
+  bool _showProcessingCircle = false;
+  int _currentIndex = 0;
+
+  final TextEditingController _selectedBankController = TextEditingController();
+  final TextEditingController _accountNumberController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration.zero, () {
+      _showBankSelectionDialog(context);
+    });
+  }
+
+  void _showBankSelectionDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return _buildBankSelectionDialog(context);
+      },
+    ).then((selectedBank) {
+      if (selectedBank != null) {
+        setState(() {
+          _bankSelected = true;
+        });
+      }
+    });
+  }
+
+  Widget _buildBankSelectionDialog(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Select Bank'),
+      ),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          _buildBankSearchTextField(),
+          Expanded(
+            child: _buildBankList(),
+          ),
+        ],
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+
+  Widget _buildBankSearchTextField() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextField(
+        decoration: const InputDecoration(
+          labelText: 'Search all banks',
+          prefixIcon: Icon(Icons.search),
+          border: OutlineInputBorder(),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: AppColors.lightBlue),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: AppColors.lightBlue),
+          ),
+          contentPadding: EdgeInsets.symmetric(horizontal: 14.0),
+        ),
+        onChanged: (text) {},
+      ),
+    );
+  }
+
+  Widget _buildBankList() {
+    return ListView.builder(
+      itemCount: dummyBanks.length,
+      itemBuilder: (BuildContext context, int index) {
+        DummyBank bank = dummyBanks[index];
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 1.0),
+          child: ListTile(
+            leading: Image.asset(
+              bank.logo,
+              width: 40,
+            ),
+            title: Text(
+              bank.name,
+              style: TextStyle(
+                fontWeight: AppFontWeight.bold,
+                fontFamily: GoogleFonts.alegreyaSans().fontFamily,
+              ),
+            ),
+            onTap: () {
+              _selectedBankController.text = bank.name;
+              _selectedBankLogo = bank.logo;
+              _userName = "";
+              Navigator.of(context).pop(bank);
+            },
+          ),
+        );
+      },
+    );
+  }
+
+  PreferredSizeWidget? _buildAppBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back),
+        onPressed: () {
+          Navigator.pushNamed(context, "/home");
+        },
+      ),
+      title: const Text(
+        "Raised Payment",
+        style: TextStyle(color: AppColors.lightGreen),
+      ),
+    );
+  }
+
+  Widget _buildRecipientCard(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10.0),
+        child: Card(
+          color: AppColors.lightBlue,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(6.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: Text(
+                    'Recipient Account',
+                    style: TextStyle(
+                      color: AppColors.lightBlack,
+                      fontWeight: AppFontWeight.bold,
+                      fontSize: AppFontSize.size18,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Container(
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(
+                        color: AppColors.lightGrey,
+                        width: 1.0,
+                      ),
+                    ),
+                  ),
+                  child: Row(
+                    children: [
+                      _selectedBankLogo.isEmpty
+                          ? Container()
+                          : Image.asset(_selectedBankLogo, width: 30.0, height: 30.0),
+                      const SizedBox(width: 8.0),
+                      Expanded(
+                        child: GestureDetector(
+                          onTap: () {
+                            _showBankSelectionDialog(context);
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                _selectedBankController.text,
+                                style: const TextStyle(
+                                  color: AppColors.lightBlack,
+                                  fontWeight: AppFontWeight.bold,
+                                  fontSize: AppFontSize.size18,
+                                ),
+                              ),
+                              const Icon(
+                                Icons.keyboard_arrow_down_sharp,
+                                size: AppFontSize.size30,
+                                color: AppColors.darkWhite,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                _buildAccountNumberTextField(),
+                _buildAccountNumberErrorMessages(),
+                _buildProcessingCircle(),
+                _buildUserNameText(),
+                const SizedBox(height: 2),
+                _buildNextTextButton()
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBankNetworkMonitorCard(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 6.0),
+      child: Card(
+        color: AppColors.lightBlue,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: ListTile(
+          onTap: () {},
+          title: const Text(
+            'Real-time Bank Network Monitor',
+            style: TextStyle(
+              color: AppColors.lightBlack,
+              fontWeight: AppFontWeight.bold,
+              fontSize: AppFontSize.size14,
+            ),
+          ),
+          trailing: const Icon(Icons.arrow_forward_ios, color: AppColors.darkWhite, size: AppFontSize.size20),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBeneficiariesCard(BuildContext context) {
+    List<DummyBank> beneficiaries = dummyBanks.sublist(0, 4);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 6.0),
+      child: Card(
+        color: AppColors.lightBlue,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              title: Row(
+                children: [
+                  const Expanded(
+                    child: Text(
+                      'Beneficiaries',
+                      style: TextStyle(
+                        color: AppColors.lightGreen,
+                        fontWeight: AppFontWeight.bold,
+                        fontSize: AppFontSize.size12,
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushNamed(context, "/show_all_beneficiaries");
+                    },
+                    child: const Row(
+                      children: [
+                        Text(
+                          'View All',
+                          style: TextStyle(
+                            color: AppColors.lightGrey,
+                            fontWeight: AppFontWeight.bold,
+                            fontSize: AppFontSize.size12,
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        Icon(
+                          Icons.arrow_forward_ios,
+                          color: AppColors.darkWhite,
+                          size: AppFontSize.size12,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              children: List<Widget>.generate(beneficiaries.length * 2 - 1, (index) {
+                if (index.isOdd) {
+                  return const Padding(
+                    padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                    child: Divider(
+                      height: 0.5,
+                      color: AppColors.darkWhite,
+                      thickness: 2,
+                    ),
+                  );
+                } else {
+                  final int beneficiaryIndex = index ~/ 2;
+                  DummyBank beneficiary = beneficiaries[beneficiaryIndex];
+                  return ListTile(
+                    leading: SizedBox(
+                      width: 30.0,
+                      height: 30.0,
+                      child: Image.asset(
+                        beneficiary.logo,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                    title: Text(
+                      beneficiary.accountName,
+                      style: const TextStyle(
+                        fontWeight: AppFontWeight.bold,
+                        fontSize: AppFontSize.size14,
+                      ),
+                    ),
+                    subtitle: Text(
+                      beneficiary.name,
+                      style: const TextStyle(
+                        color: AppColors.lightGreen,
+                        fontSize: AppFontSize.size12,
+                      ),
+                    ),
+                  );
+                }
+              }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildAccountNumberTextField() {
+    bool isAccountNumberEmpty = _accountNumberController.text.isEmpty;
+    bool isAccountNumberInvalid = _accountNumberController.text.length < 10;
+
+    String labelText = isAccountNumberEmpty ? 'Enter Account Number' : '';
+
+    UnderlineInputBorder customErrorBorder = UnderlineInputBorder(
+      borderSide: BorderSide(
+        color: (isAccountNumberEmpty || isAccountNumberInvalid) ? AppColors.errorRed : AppColors.darkWhite.withOpacity(0.5),
+      ),
+    );
+
+    UnderlineInputBorder customFocusedErrorBorder = const UnderlineInputBorder(
+      borderSide: BorderSide(
+        color: AppColors.errorRed,
+      ),
+    );
+
+    return TextFormField(
+      controller: _accountNumberController,
+      decoration: InputDecoration(
+        labelText: labelText,
+        labelStyle: const TextStyle(
+          color: AppColors.darkWhite,
+          fontWeight: AppFontWeight.bold,
+          fontSize: AppFontSize.size16,
+        ),
+        enabledBorder: UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: (_isAccountNumberErrorVisible || _isAccountNumberLengthInvalid) ? AppColors.errorRed : AppColors.darkWhite.withOpacity(0.5),
+          ),
+        ),
+        focusedBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: AppColors.darkWhite),
+        ),
+        errorBorder: customErrorBorder,
+        focusedErrorBorder: customFocusedErrorBorder,
+        contentPadding: const EdgeInsets.only(
+          top: 4.0,
+          bottom: 4.0,
+        ),
+        fillColor: AppColors.lightBlue,
+        floatingLabelBehavior: FloatingLabelBehavior.never,
+      ),
+      inputFormatters: [LengthLimitingTextInputFormatter(10)],
+      onChanged: (value) {
+        setState(() {
+          if (value.isEmpty) {
+            _isAccountNumberErrorVisible = true;
+            _isAccountNumberLengthInvalid = false;
+            _userName = '';
+          } else if (value.length != 10) {
+            _isAccountNumberErrorVisible = false;
+            _isAccountNumberLengthInvalid = true;
+            _userName = '';
+          } else {
+            _showProcessingCircle = true;
+            _isAccountNumberErrorVisible = false;
+            _isAccountNumberLengthInvalid = false;
+            Future.delayed(const Duration(seconds: 3), () {
+              DummyBank matchingBank;
+              try {
+                matchingBank = dummyBanks.firstWhere((bank) => bank.accountNumber == value && bank.name == _selectedBankController.text);
+                _userName = matchingBank.accountName;
+              } catch (e) {
+                _isAccountNumberErrorVisible = true;
+                _userName = 'Account not found';
+              } finally {
+                _showProcessingCircle = false;
+              }
+              setState(() {});
+            });
+          }
+        });
+      },
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Account number should not be empty';
+        } else if (value.length < 10) {
+          return 'Account length is invalid';
+        }
+        return null;
+      },
+    );
+  }
+
+  Widget _buildAccountNumberErrorMessages() {
+    const TextStyle errorTextStyle = TextStyle(
+      color: AppColors.errorRed,
+      fontWeight: AppFontWeight.medium,
+      fontSize: AppFontSize.size14,
+    );
+    return Column(
+      children: [
+        Visibility(
+          visible: _isAccountNumberErrorVisible && _accountNumberController.text.isEmpty,
+          child: const Text(
+            'Account number is empty',
+            style: errorTextStyle,
+          ),
+        ),
+        Visibility(
+          visible: _isAccountNumberLengthInvalid,
+          child: const Text(
+            'Account length is invalid',
+            style: errorTextStyle,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildProcessingCircle() {
+    return Visibility(
+      visible: _showProcessingCircle,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 39.0, bottom: 0.12),
+        child: Column(
+          children: [
+            const Center(
+              child: SizedBox(
+                width: 20.0,
+                height: 20.0,
+                child: CircularProgressIndicator(),
+              ),
+            ),
+            Text(
+              _userName,
+              style: const TextStyle(
+                color: AppColors.lightGreen,
+                fontWeight: AppFontWeight.bold,
+                fontSize: AppFontSize.size16,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserNameText() {
+    return Center(
+      child: Transform.translate(
+        offset: const Offset(0, 9.0),
+        child: Text(
+          _userName,
+          style: const TextStyle(
+            color: AppColors.lightGreen,
+            fontWeight: AppFontWeight.bold,
+            fontSize: AppFontSize.size20,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNextTextButton() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20.0),
+      child: Center(
+        child: SizedBox(
+          height: 50,
+          width: 100.0,
+          child: ElevatedButton(
+            onPressed: () {
+              if (_bankSelected) {
+                final selectedBank = dummyBanks.firstWhere((bank) => bank.name == _selectedBankController.text);
+                if (selectedBank != null) {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => RaisePaymentBeneficiaryDetailScreen(
+                        bank: selectedBank,
+                        selectedBankLogo: _selectedBankLogo,
+                      ),
+                    ),
+                  );
+                } else {
+                  print('Selected bank not found');
+                }
+              }
+            },
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(vertical: 15.0),
+              backgroundColor: AppColors.lightGreen,
+            ),
+            child: const Text(
+              'Next',
+              style: TextStyle(
+                fontWeight: AppFontWeight.bold,
+                color: AppColors.pureWhite,
+                fontSize: AppFontSize.size16,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(context),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildRecipientCard(context),
+            _buildBankNetworkMonitorCard(context),
+            _buildBeneficiariesCard(context),
+          ],
+        ),
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/raise_payment/raise_payment_detail.dart
@@ -0,0 +1,289 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:pay_me_mobile/screens/transfer_to_bank/transfer_to_bank_screen.dart';
+import 'package:pay_me_mobile/views/custom/show_pin_dialog.dart';
+import '../../app_config/manager/font_manager.dart';
+import '../../app_config/manager/theme_manager.dart';
+import '../../views/custom/custom_dynamic_label_textfield.dart';
+import '../../views/custom/custom_underline.dart';
+
+class RaisePaymentBeneficiaryDetailScreen extends StatefulWidget {
+  final DummyBank bank;
+  final String selectedBankLogo;
+
+  const RaisePaymentBeneficiaryDetailScreen({Key? key, required this.bank, required this.selectedBankLogo}) : super(key: key);
+
+  @override
+  _RaisePaymentBeneficiaryDetailScreenState createState() => _RaisePaymentBeneficiaryDetailScreenState();
+}
+
+class _RaisePaymentBeneficiaryDetailScreenState extends State<RaisePaymentBeneficiaryDetailScreen> {
+  final TextEditingController amountController = TextEditingController();
+  final TextEditingController narrationController = TextEditingController();
+
+  String emptyAmountErrorMessage = 'Amount is empty';
+  String emptyNarrativeMessage = 'Narration field cannot be empty';
+
+
+  @override
+  void initState() {
+    super.initState();
+    amountController.addListener(amountErrorHandling);
+    narrationController.addListener(narrationErrorHandling);
+  }
+
+  @override
+  void dispose() {
+    amountController.dispose();
+    narrationController.dispose();
+    super.dispose();
+  }
+
+  void narrationErrorHandling() {
+    if (narrationController.text.isEmpty) {
+      setState(() {
+        emptyNarrativeMessage = 'Narration field cannot be empty';
+      });
+    } else if (narrationController.text.length < 3) {
+      setState(() {
+        emptyNarrativeMessage = 'Narration must be at least 3 characters';
+      });
+    } else {
+      setState(() {
+        emptyNarrativeMessage = '';
+      });
+    }
+  }
+
+  void amountErrorHandling() {
+    double enteredAmount = double.tryParse(amountController.text) ?? 0.0;
+
+    if (amountController.text.isEmpty) {
+      setState(() {
+        emptyAmountErrorMessage = 'Amount is empty';
+      });
+    } else {
+      setState(() {
+        emptyAmountErrorMessage = '';
+      });
+    }
+  }
+
+  Widget buildBankCard() {
+    return Card(
+      color: AppColors.lightBlue,
+      elevation: 0.0,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              color: AppColors.lightBlue,
+              child: Image.asset(
+                widget.bank.logo,
+                width: 40,
+                height: 60,
+              ),
+            ),
+            const SizedBox(width: 30),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  widget.bank.accountName,
+                  style: CustomStyles.bankInfoTextStyle,
+                ),
+                Text(
+                  widget.bank.accountNumber,
+                  style: CustomStyles.bankInfoTextStyle,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildSendMoneySection() {
+    return const Column(
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: CustomUnderlinedText(
+            text: "Send Money",
+            textStyle: TextStyle(
+              fontSize: AppFontSize.size24,
+              fontWeight: AppFontWeight.bold,
+              color: AppColors.lightGreen,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildAmountInput() {
+    return Row(
+      children: [
+        Expanded(
+          child: Container(
+            color: AppColors.deepWhite,
+            child: TextFieldWithDynamicLabel(
+              controller: amountController,
+              labelText: '₦ 0.00',
+              onChanged: (value) {
+                amountErrorHandling();
+              },
+              onValidation: (value) {},
+              hintText: '',
+              fillColor: AppColors.deepWhite,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildAmountErrorMessage() {
+    if (emptyAmountErrorMessage.isNotEmpty) {
+      return Padding(
+        padding: const EdgeInsets.only(top: 8.0),
+        child: Text(
+          emptyAmountErrorMessage,
+          style: const TextStyle(
+            color: AppColors.errorRed,
+          ),
+        ),
+      );
+    } else {
+      return const SizedBox.shrink();
+    }
+  }
+
+  Widget buildBalanceSection() {
+    return Column(
+      children: [
+        TextField(
+          controller: narrationController,
+          decoration: InputDecoration(
+            labelText: 'Enter Narration',
+            labelStyle: const TextStyle(color: AppColors.lightBlack, fontSize: AppFontSize.size7),
+            filled: true,
+            fillColor: AppColors.lightBlue,
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: const BorderSide(
+                  color: AppColors.lightBlue, width: 2.0
+              ),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: const BorderSide(
+                  color: AppColors.lightBlue, width: 2.0
+              ),
+            ),
+          ),
+          onChanged: (value) {
+            narrationErrorHandling();
+          },
+        ),
+        if (emptyNarrativeMessage.isNotEmpty)
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0),
+            child: Text(
+              emptyNarrativeMessage,
+              style: const TextStyle(
+                color: AppColors.errorRed,
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+
+  Widget buildSubmitButton() {
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 50.0, left: 5.0),
+          child: SizedBox(
+            width: 150,
+            child: ElevatedButton(
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return PinDialog(
+                      amount: double.parse(amountController.text),
+                      accountName: widget.bank.accountName,
+                    );
+                  },
+                );
+              },
+              child: const Text(
+                'Submit',
+                style: TextStyle(
+                  color: AppColors.deepWhite,
+                  fontSize: AppFontSize.size20,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 50.0, left: 55),
+          child: SizedBox(
+            width: 150,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: const Text(
+                'Cancel',
+                style: TextStyle(
+                  color: AppColors.deepWhite,
+                  fontSize: AppFontSize.size20,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              buildBankCard(),
+              const SizedBox(height: 50),
+              buildSendMoneySection(),
+              const SizedBox(height: 50),
+              buildAmountInput(),
+              buildAmountErrorMessage(),
+              const SizedBox(height: 50),
+              buildBalanceSection(),
+              buildSubmitButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomStyles {
+  static const TextStyle bankInfoTextStyle = TextStyle(
+    fontSize: AppFontSize.size24,
+    color: AppColors.lightBlack,
+    fontWeight: AppFontWeight.bold,
+  );
+}
