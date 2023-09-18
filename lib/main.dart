e34a242c809cabe5135c2fcc426212dc0072e4e4
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-18 23:09:12 +0000

    saved and stored user token

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:pay_me_mobile/views/token/UserToken.dart';
+import 'package:provider/provider.dart';
 
 import 'app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      ChangeNotifierProvider(
+          create: (_) => UserToken(),
+          child:const MyApp()
+      )
+  );
 }
