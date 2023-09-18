926f03534719509d10188f2c5a33d8f91f71f2e3
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-18 23:02:18 +0000

    one time login, next time set pass code

--- lib/controllers/splash/Splash_controller.dart
+++ lib/controllers/splash/Splash_controller.dart
@@ -1,12 +1,21 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:pay_me_mobile/views/auth_view/pass_code.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../views/auth_view/login_view.dart';
 
 class SplashController {
-  void initialize(BuildContext context) {
+  Future<void> initialize(BuildContext context) async {
+    final prefs = await SharedPreferences.getInstance();
+    final bool hasLoggedIn = prefs.getBool('hasLoggedIn') ?? false;
+
     Timer(const Duration(milliseconds: 900), () {
-      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const LoginScreen()));
+      if (hasLoggedIn) {
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const PassCodeScreen()));
+      } else {
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (_) => const LoginScreen()));
+      }
     });
   }
 }
