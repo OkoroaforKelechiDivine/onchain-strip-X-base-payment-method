83a1b6fae4dd1d2b4e59dd747fa724052f5d8ec4
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-19 16:33:35 +0000

    added notifier for token provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:pay_me_mobile/views/token/UserToken.dart';
+import 'package:pay_me_mobile/token/token_provider.dart';
 import 'package:provider/provider.dart';
 
 import 'app.dart';
 
 void main() {
   runApp(
-      ChangeNotifierProvider(
-          create: (_) => UserToken(),
-          child:const MyApp()
-      )
+    ChangeNotifierProvider(
+      create: (context) => TokenProvider(),
+      child: const MyApp(),
+    ),
   );
 }
