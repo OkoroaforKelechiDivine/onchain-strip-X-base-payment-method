13b35c05bc2563a35d36e29b7a4227fce3e56016
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-19 16:31:24 +0000

    changed route to login screen

--- lib/controllers/splash/Splash_controller.dart
+++ lib/controllers/splash/Splash_controller.dart
@@ -1,21 +1,13 @@
+
 import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:pay_me_mobile/views/auth_view/pass_code.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import '../../views/auth_view/login_view.dart';
 
-class SplashController {
-  Future<void> initialize(BuildContext context) async {
-    final prefs = await SharedPreferences.getInstance();
-    final bool hasLoggedIn = prefs.getBool('hasLoggedIn') ?? false;
 
+class SplashController {
+  void initialize(BuildContext context) {
     Timer(const Duration(milliseconds: 900), () {
-      if (hasLoggedIn) {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const PassCodeScreen()));
-      } else {
-        Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (_) => const LoginScreen()));
-      }
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_) => const LoginScreen()));
     });
   }
 }
