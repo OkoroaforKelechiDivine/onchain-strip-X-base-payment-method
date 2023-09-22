a0c0fff0c29bce608e49985e583df3b721ab5a3d
theRealNonso <okoroaforkelechi123@gmail.com>
2023-09-22 00:24:03 +0000

    MADE CHANGES

---
+++ lib/data/data_provider/transfer_data_provider.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+
+import '../constants/api_routes.dart';
+import '../constants/enum/request_type.dart';
+import '../model/auth/app_response.dart';
+import '../network_manager/network_manager.dart';
+
+class TransferDataProvider {
+  NetworkManager networkManager = NetworkManager();
+
+  Future<AppResponse> fetchBankList(BuildContext context) async {
+    var completer = Completer<AppResponse>();
+    try {
+      Map<String, dynamic>? response = await networkManager.networkRequestManager(
+        RequestType.GET,
+        ApiRoutes.getAllBanks,
+        useAuth: true,
+        retrieveResponse: true,
+        retrieveUnauthorizedResponse: false,
+      );
+
+      if (response != null) {
+        if (response.containsKey('data') && response['data'] is List) {
+          List<dynamic> bankList = response['data'];
+          List<Bank> banks = bankList.map((bankJson) => Bank.fromJson(bankJson)).toList();
+          completer.complete();
+        } else {
+          completer.completeError("Invalid response format.");
+        }
+      } else {
+        completer.completeError("Network error or no response received.");
+      }
+    } catch (e) {
+      completer.completeError(e);
+    }
+    return completer.future;
+  }
+
+}
+
+  class Bank {
+  final String name;
+  final String logo;
+  final int id;
+  final String code;
+
+  Bank(this.name, this.logo, this.id, this.code);
+
+  factory Bank.fromJson(Map<String, dynamic> json) {
+    return Bank(
+      json['name'] as String,
+      json['logo'] as String,
+      json['id'] as int,
+      json['code'] as String,
+    );
+  }
+}
---
+++ lib/data/view_models/transaction /transfer_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/cupertino.dart';
+import 'package:pay_me_mobile/data/data_provider/transfer_data_provider.dart';
+import 'package:pay_me_mobile/data/states/transaction%20/transfer_state.dart';
+import '../../../locator.dart';
+import '../../constants/enum/view_state.dart';
+import '../../model/auth/app_response.dart';
+import '../../utilities/secure_storage/secure_storage_utils.dart';
+
+class TransferModel extends TransferState {
+  final TransferDataProvider _transferDataProvider = locator<TransferDataProvider>();
+  String? _message;
+
+  String? get message => _message;
+
+  fetchBankList(BuildContext context) async {
+    setState(ViewState.Busy);
+    try {
+      final response = await _transferDataProvider.fetchBankList(context);
+      SecureStorageUtils.saveToken(token: response.token ?? "null");
+      setState(ViewState.Retrieved);
+    } catch (e) {
+      _message = "NO BANK WAS FOUND";
+      setState(ViewState.Error);
+    }
+  }
+}
