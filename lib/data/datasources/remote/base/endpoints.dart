64e6707ad25d5ae8c0ebddc30d24374d895ef219
israelglory <iglory6@gmail.com>
2023-12-22 16:33:12 +0100

    live

--- lib/data/datasources/remote/base/endpoints.dart
+++ lib/data/datasources/remote/base/endpoints.dart
@@ -1,5 +1,5 @@
 class Endpoints {
-  static const baseUrl = 'https://payme-api-dev.systemshift.africa';
+  //static const baseUrl = 'https://payme-api-dev.systemshift.africa';
   //LiVE ENDPOINT
-  //static const baseUrl = 'https://payme-api.systemshift.africa';
+  static const baseUrl = 'https://payme-api.systemshift.africa';
 }
