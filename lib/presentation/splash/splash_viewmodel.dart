60a3595ee9699829efe1f2245ce0068fe91506b3
theRealNonso <okoroaforkelechi123@gmail.com>
2024-05-07 13:36:56 +0000

    changed route to intro screen

--- lib/presentation/splash/splash_viewmodel.dart
+++ lib/presentation/splash/splash_viewmodel.dart
@@ -1,10 +1,10 @@
 import 'package:pay_me_mobile/core/di/locator.dart';
-import 'package:pay_me_mobile/presentation/bottom_nav.dart';
+import 'package:pay_me_mobile/presentation/intro/intro_screen.dart';
 import 'package:stacked/stacked.dart';
 
 class SplashScreenViewModel extends BaseViewModel {
   Future<void> initializeSplashScreen() async {
     await Future.delayed(const Duration(seconds: 5));
-    navigationService.pushAndRemoveUntil(const BottomNav());
+    navigationService.pushAndRemoveUntil(const IntroScreen());
   }
 }
